53161bdc7e0bc6102827ac84cbd84cf63b9e58b1
Oleg Semiashkin <olegsemyashkin197@gmail.com>
2025-05-25 00:28:49 +0300

    Update ip-bulk.js

--- pages/ip-bulk.js
+++ pages/ip-bulk.js
@@ -1,42 +1,60 @@
-// pages/ip-bulk.js
-
 import React from 'react';
 
 export default function BulkIP({ ips }) {
   return (
-    <div style={{ margin: "0 auto", maxWidth: 1200 }}>
-      <table className="bulk-table">
-        <thead>
-          <tr>
-            <th>IP</th>
-            <th>Country</th>
-            <th>City</th>
-            <th>VPN/Proxy</th>
-            <th>Org/ISP</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ips && ips.length ? ips.map((ip, i) => (
-            <tr key={ip.address || i}>
-              <td>{ip.address}</td>
-              <td>{ip.country}</td>
-              <td>{ip.city}</td>
-              <td style={{ color: (ip.proxy || ip.vpn) ? '#e95a16' : '#2b7b2b', fontWeight: 600 }}>
-                {(ip.proxy || ip.vpn) ? (ip.proxy ? "Proxy" : "VPN") : "Clean"}
-              </td>
-              <td>{ip.isp}</td>
-              <td>
-                <a href={`/details/${ip.address}`} style={{ color: "#e95a16" }}>▶ Details</a>
-              </td>
-            </tr>
-          )) : (
+    <div style={{ margin: "0 auto", maxWidth: "98vw", padding: 0 }}>
+      <div style={{ overflowX: "auto" }}>
+        <table className="bulk-table" style={{ minWidth: 1050, width: "100%", borderCollapse: "separate" }}>
+          <colgroup>
+            <col style={{ width: "15%" }} />   {/* IP */}
+            <col style={{ width: "11%" }} />   {/* Country */}
+            <col style={{ width: "12%" }} />   {/* City */}
+            <col style={{ width: "8%" }} />    {/* Proxy */}
+            <col style={{ width: "8%" }} />    {/* VPN */}
+            <col style={{ width: "8%" }} />    {/* TOR */}
+            <col style={{ width: "28%" }} />   {/* Org/ISP */}
+            <col style={{ width: "10%" }} />   {/* Details */}
+          </colgroup>
+          <thead>
             <tr>
-              <td colSpan={6}>No data</td>
+              <th style={{ textAlign: "left" }}>IP</th>
+              <th style={{ textAlign: "left" }}>Country</th>
+              <th style={{ textAlign: "left" }}>City</th>
+              <th style={{ textAlign: "left" }}>Proxy</th>
+              <th style={{ textAlign: "left" }}>VPN</th>
+              <th style={{ textAlign: "left" }}>TOR</th>
+              <th style={{ textAlign: "left" }}>Org/ISP</th>
+              <th style={{ textAlign: "left" }}>Details</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {ips && ips.length ? ips.map((ip, i) => (
+              <tr key={ip.address || i}>
+                <td>{ip.address}</td>
+                <td>{ip.country}</td>
+                <td>{ip.city}</td>
+                <td style={{ color: ip.proxy ? '#e95a16' : '#2b7b2b', fontWeight: 600 }}>
+                  {ip.proxy ? 'Yes' : 'No'}
+                </td>
+                <td style={{ color: ip.vpn ? '#e95a16' : '#2b7b2b', fontWeight: 600 }}>
+                  {ip.vpn ? 'Yes' : 'No'}
+                </td>
+                <td style={{ color: ip.tor ? '#e95a16' : '#2b7b2b', fontWeight: 600 }}>
+                  {ip.tor ? 'Yes' : 'No'}
+                </td>
+                <td>{ip.isp}</td>
+                <td>
+                  <a href={`/details/${ip.address}`} style={{ color: "#e95a16" }}>▶ Details</a>
+                </td>
+              </tr>
+            )) : (
+              <tr>
+                <td colSpan={8}>No data</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
