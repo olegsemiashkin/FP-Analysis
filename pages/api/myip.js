4909c70b8f96819a4016160898645422061ee28c
Oleg Semiashkin <olegsemyashkin197@gmail.com>
2025-05-24 21:37:55 +0300

    Update myip.js

--- pages/api/myip.js
+++ pages/api/myip.js
@@ -1,1 +1,7 @@
-
+export default async function handler(req, res) {
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
+    req.socket?.remoteAddress ||
+    null;
+  res.status(200).json({ ip });
+}
