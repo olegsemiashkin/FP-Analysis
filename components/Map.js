6eab934bb2a24124f66ff87f48a033354927dd40
Oleg Semiashkin <olegsemyashkin197@gmail.com>
2025-05-25 00:20:27 +0300

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,20 +1,27 @@
-// components/Map.js
-
 import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet';
 
 export default function Map({ lat, lon, city }) {
   return (
     <div style={{
       background: "#fff1e6",
-      border: "2px solid #3793ec",
+      border: "2.5px solid #3793ec",
       borderRadius: 18,
-      padding: 8,
-      margin: "0 auto"
+      padding: 10,
+      margin: "0 auto",
+      minWidth: 220,
+      minHeight: 145,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
     }}>
       <MapContainer
         center={[lat, lon]}
-        zoom={10} // Уменьшил для охвата города!
-        style={{ height: 180, width: 320 }}
+        zoom={10}
+        style={{
+          height: 135,
+          width: 240,
+          background: "#fff"
+        }}
         scrollWheelZoom={false}
         dragging={false}
         doubleClickZoom={false}
@@ -25,7 +32,7 @@ export default function Map({ lat, lon, city }) {
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         <Marker position={[lat, lon]} />
-        <Circle center={[lat, lon]} radius={2500} pathOptions={{ color: "#e95a16", fillOpacity: 0.2 }} />
+        <Circle center={[lat, lon]} radius={2500} pathOptions={{ color: "#e95a16", fillOpacity: 0.18 }} />
       </MapContainer>
     </div>
   );
